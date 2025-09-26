cc440af7803510d0296a00670f6ec4c7da1e55a1
unknown <benjamin.coombe@mymail.champlain.edu>
2025-09-25 21:41:53 -0400

    assignment 3.1 done

--- week3/dns-resolver.ps1
+++ week3/dns-resolver.ps1
@@ -1,1 +1,6 @@
-Powershell test file
+param($NetworkPrefix, $DnsServer)
+$base = $NetworkPrefix -replace '\d+/\d+$', ''
+1..254 | ForEach-Object { 
+    $result = Resolve-DnsName -DnsOnly "$base$_" -Server $DnsServer -ErrorAction Ignore
+    if ($result) { "$base$_ -> $($result.NameHost)" }
+}
